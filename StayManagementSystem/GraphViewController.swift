ed31af0ebcac1f07e4b8f8c504f484d6e4f48579
Kyohei Sakai <nico_f00tb@yahoo.co.jp>
2016-09-06 23:09:11 +0900

    Improve GraphViewController
    To use ScrollView

--- StayManagementSystem/GraphViewController.swift
+++ StayManagementSystem/GraphViewController.swift
@@ -10,116 +10,177 @@ import UIKit
 
 class GraphViewController: UIViewController {
     
-    let size: CGSize = UIScreen.mainScreen().nativeBounds.size
+    let size: CGSize = UIScreen.mainScreen().bounds.size
 //    let view = UIView(frame: CGRect(origin: CGPointZero, size: size))
 //    view.backgroundColor = UIColor(white: 1.0, alpha: 1.0)
     
-    func drawLine(from: CGPoint, to: CGPoint) {
-        let linePath = UIBezierPath()
-        linePath.moveToPoint(from)
-        linePath.addLineToPoint(to)
-        linePath.lineWidth = 0.5
-        UIColor.grayColor().setStroke()
-        linePath.stroke()
-    }
-    
-    func drawBar(from: CGPoint, to: CGPoint, width: CGFloat) {
-        let BarPath = UIBezierPath()
-        BarPath.moveToPoint(from)
-        BarPath.addLineToPoint(to)
-        BarPath.lineWidth = width
-        UIColor.blueColor().setStroke()
-        BarPath.stroke()
-    }
-    
-    // 文字列を描画
-    func drawString(word: String, fontSize: CGFloat, drawPoint: CGPoint){
-        let attrString = NSAttributedString(
-            string: word,
-            attributes:[NSForegroundColorAttributeName: UIColor.blackColor(),
-                NSFontAttributeName: UIFont.boldSystemFontOfSize(fontSize)])
-        attrString.drawAtPoint(drawPoint)
-        
-    }
+    var scrollView: UIScrollView = UIScrollView()
     
     
     var dataArray: [(name: String, start: CGFloat, time: CGFloat)] = [
-        ("田中", 2, 4), ("健太", 1, 3), ("典子", 0, 5), ("加藤", 2, 8), ("ザッキー", 4, 15),
-        ("えび蔵", 0, 10), ("Jack", 3.5, 7.7)
-    ]
-
-
+        ("H", 2, 4), ("M", 1, 3), ("T", 0, 5), ("R", 2, 8), ("A", 4, 15),
+        ("S", 0, 10), ("Y", 3.5, 7.7), ("H", 2, 4), ("M", 1, 3), ("T", 0, 5), ("R", 2, 8), ("A", 4, 15),
+        ]
+    
+    let xMargin: CGFloat = 50  // 左の余白幅
+    let yMargin: CGFloat = 50  // 上の余白幅
+    let xScaleInterval: CGFloat = 60 // グラフ表示部分の幅
+    let yScaleInterval: CGFloat = 50    // 目盛りの幅
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // CoreGraphicsで描画する
-        UIGraphicsBeginImageContextWithOptions(size, false, 0)
+        scrollView.frame = CGRectMake(0, 0, size.width, size.height)
+        scrollView.backgroundColor = UIColor.whiteColor()
+        print(size)
         
+        // 十分な描画領域を確保
+        let canvasSize = CGSize(width: size.width * 3, height: size.height * 3)
         
-        // 軸となる線
-        let axisline = UIBezierPath()
-        let yMargin: CGFloat = 100  // 上の余白幅
-        axisline.moveToPoint(CGPointMake(yMargin, 0))
-        axisline.addLineToPoint(CGPointMake(yMargin, size.height))
-        UIColor.grayColor().setStroke()
-        axisline.lineWidth = 2
-        axisline.stroke()
+        let line: Line = Line(dataArray: dataArray)
+        line.frame = CGRectMake(0, 0, canvasSize.width, canvasSize.height)
+        line.backgroundColor = UIColor.clearColor()
+        scrollView.addSubview(line)
         
-        // 仕切り線
-        let partitionline = UIBezierPath()
-        let xMargin: CGFloat = 100  // 左の余白幅
-        partitionline.moveToPoint(CGPointMake(0, xMargin))
-        partitionline.addLineToPoint(CGPointMake(size.width, xMargin))
-        UIColor.grayColor().setStroke()
-        partitionline.lineWidth = 2
-        partitionline.stroke()
+        let bar: Bar = Bar(dataArray: dataArray)
+        bar.frame = CGRectMake(0, 0, canvasSize.width, canvasSize.height)
+        bar.backgroundColor = UIColor.clearColor()
+        scrollView.addSubview(bar)
         
-        // 目盛り線
-        let lineNum: Int = 12   // 目盛りの本数
-        let startTime: Int = 0 // "startTime":00
-        for i in 1...lineNum {
-            let xToEnd = size.width
-            // 目盛りの幅
-            let yScaleInterval = (size.height - yMargin) / CGFloat(lineNum + 1)
-            // 目盛りのY座標を求める
-            let yScalePoint = yMargin + yScaleInterval * CGFloat(i)
-            
-            drawLine(CGPoint(x: xMargin, y: yScalePoint), to: CGPoint(x: xToEnd, y: yScalePoint))
-            
-            // 目盛り線にラベルをつける
-            drawString("\(Int(startTime) + i)" + ":00", fontSize: 20.0, drawPoint: CGPoint(x: xMargin - 70, y: yScalePoint - 10))
-        }
+        scrollView.contentSize = CGSizeMake(line.xLength, line.yLength)
+        print(scrollView.contentSize)
+        view.addSubview(scrollView)
         
-        // グラフ
-        let BarNum: Int = dataArray.count
-        for i in 1...BarNum {
-            // グラフの間隔
-            let xScaleInterval = (size.width - xMargin) / CGFloat(BarNum + 1)
-            // グラフのX座標を求める
-            let xScalePoint = xMargin + xScaleInterval * CGFloat(i)
-            let barWidth = (xScaleInterval / CGFloat(5) * CGFloat(2)) * CGFloat(2)
-            //グラフの始点と終点
-            let yScaleInterval = (size.height - yMargin) / CGFloat(lineNum + 1)
-            let yFromPoint = yMargin + yScaleInterval * dataArray[i - 1].start
-            let yToPoint = yFromPoint + yScaleInterval * dataArray[i - 1].time
-            
-            drawBar(CGPoint(x: xScalePoint, y: yFromPoint), to: CGPoint(x: xScalePoint, y: yToPoint), width: barWidth)
-            
-            // グラフに名前をつける
-            drawString(dataArray[i - 1].name, fontSize: 20.0, drawPoint: CGPoint(x: xScalePoint - 30, y: yMargin - 45))
-        }
-        
-        
-        // viewのlayerに描画したものをセットする
-        view.layer.contents = UIGraphicsGetImageFromCurrentImageContext().CGImage
-        
-        UIGraphicsEndImageContext()
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    class Line: UIView {
+        var xLength: CGFloat = 0.0
+        var yLength: CGFloat = 0.0
+        var xMargin: CGFloat = 50
+        var yMargin: CGFloat = 50
+        let xScaleInterval: CGFloat = 60 // グラフ表示部分の幅
+        let yScaleInterval: CGFloat = 50    // 目盛りの幅
+        var dataArray: [(name: String, start: CGFloat, time: CGFloat)] = []
+        
+        convenience init (dataArray: [(name: String, start: CGFloat, time: CGFloat)]) {
+            self.init()
+            self.dataArray = dataArray
+            
+            // Barを表示するための領域の高さを求める
+            var maxTime = [CGFloat]()
+            for i in 0..<dataArray.count {
+                maxTime.append(dataArray[i].start + dataArray[i].time)
+            }
+            
+            self.yLength = yMargin + yScaleInterval * maxTime.maxElement()!
+            self.xLength = xMargin + xScaleInterval * CGFloat(dataArray.count)
+        }
+        
+        
+        override func drawRect(rect: CGRect) {
+            // 仕切り線（縦）
+            drawLine(CGPointMake(yMargin, 0), to: CGPointMake(yMargin, yLength), width: 2)
+            // 仕切り線（横）
+            drawLine(CGPointMake(0, yMargin), to: CGPointMake(xLength, yMargin), width: 2)
+            
+            
+            // 目盛り線の本数を求める(floor →小数点以下切り捨て)
+            let lineNum: Int = Int(floor((yLength - yMargin) / yScaleInterval))
+            
+            let startTime: Int = 0 // "startTime":00
+            
+            for i in 1...lineNum {
+                // 目盛りのY座標を求める
+                let yScalePoint = yMargin + yScaleInterval * CGFloat(i)
+                
+                drawLine(CGPoint(x: xMargin, y: yScalePoint), to: CGPoint(x: xLength, y: yScalePoint), width: 0.5)
+                
+                // 目盛り線にラベルをつける
+                drawString("\(Int(startTime) + i)" + ":00", fontSize: 10.0, drawPoint: CGPoint(x: xMargin - 35, y: yScalePoint - 5))
+            }
+            
+        }
+        
+        func drawLine(from: CGPoint, to: CGPoint, width: CGFloat) {
+            let linePath = UIBezierPath()
+            linePath.moveToPoint(from)
+            linePath.addLineToPoint(to)
+            linePath.lineWidth = width
+            UIColor.grayColor().setStroke()
+            linePath.stroke()
+        }
+        
+        // 文字列を描画
+        func drawString(word: String, fontSize: CGFloat, drawPoint: CGPoint){
+            let attrString = NSAttributedString(
+                string: word,
+                attributes:[NSForegroundColorAttributeName: UIColor.blackColor(),
+                    NSFontAttributeName: UIFont.boldSystemFontOfSize(fontSize)])
+            attrString.drawAtPoint(drawPoint)
+        }
+        
+    }
+    
+    
+    class Bar: UIView {
+        
+        let barWidth: CGFloat = 40  // Barの幅を設定
+        var xLength: CGFloat = 0.0
+        var yLength: CGFloat = 0.0
+        var xMargin: CGFloat = 50
+        var yMargin: CGFloat = 50
+        let xScaleInterval: CGFloat = 60 // グラフ表示部分の幅
+        let yScaleInterval: CGFloat = 50    // 目盛りの幅
+        var dataArray: [(name: String, start: CGFloat, time: CGFloat)] = []
+        
+        convenience init (dataArray: [(name: String, start: CGFloat, time: CGFloat)]) {
+            self.init()
+            self.dataArray = dataArray
+        }
+        
+        override func drawRect(rect: CGRect) {
+            
+            let BarNum: Int = dataArray.count
+            
+            for i in 1...BarNum {
+                // グラフのX座標を求める
+                let xScalePoint = xMargin + (xScaleInterval * CGFloat(i)) - (xScaleInterval / 2)
+                //グラフの始点と終点
+                let yFromPoint = yMargin + yScaleInterval * dataArray[i - 1].start
+                let yToPoint = yFromPoint + yScaleInterval * dataArray[i - 1].time
+                
+                drawBar(CGPoint(x: xScalePoint, y: yFromPoint), to: CGPoint(x: xScalePoint, y: yToPoint), width: barWidth)
+                
+                // グラフに名前をつける
+                drawString(dataArray[i - 1].name, fontSize: 25.0, drawPoint: CGPoint(x: xScalePoint - barWidth / 2, y: 15))
+            }
+            
+        }
+        
+        func drawBar(from: CGPoint, to: CGPoint, width: CGFloat) {
+            let BarPath = UIBezierPath()
+            BarPath.moveToPoint(from)
+            BarPath.addLineToPoint(to)
+            BarPath.lineWidth = width
+            UIColor.blueColor().setStroke()
+            BarPath.stroke()
+        }
+        
+        // 文字列を描画
+        func drawString(word: String, fontSize: CGFloat, drawPoint: CGPoint){
+            let attrString = NSAttributedString(
+                string: word,
+                attributes:[NSForegroundColorAttributeName: UIColor.blackColor(),
+                    NSFontAttributeName: UIFont.boldSystemFontOfSize(fontSize)])
+            attrString.drawAtPoint(drawPoint)
+        }
+        
     }
 
 }
